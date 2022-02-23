3c09ddb17134c47c8a06857c8eb12d38a6428fc9
masudrana <masudranaacb@gmail.com>
2022-02-23 20:25:45 +0600

    simple fetch

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const student={
-    name:'Masud Rana',
-    adress:"Dinajpur 514",
-}
-console.log(student)
-const notString=JSON.stringify(student);
-console.log(notString);
-const convert=JSON.parse(notString);
-console.log(convert)
+// const student={
+//     name:'Masud Rana',
+//     adress:"Dinajpur 514",
+// }
+// console.log(student)
+// const notString=JSON.stringify(student);
+// console.log(notString);
+// const convert=JSON.parse(notString);
+// console.log(convert)
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(data=> console.log(data))
